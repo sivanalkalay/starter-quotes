58d783f9ff0e30db3e8c0fb57c3b6720f26f17e2
sivanalkalay <sivanalkalay@msn.com>
2016-01-30 16:17:18 -0800

    Link 2 fixed

---
+++ application/controllers/last/Welcome.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Our homepage. Show a table of all the author pictures. Clicking on one should show their quote.
+ * Our quotes model has been autoloaded, because we use it everywhere.
+ * 
+ * controllers/Welcome.php
+ *
+ * ------------------------------------------------------------------------
+ */
+class Welcome extends Application {
+
+    function __construct() 
+    {
+        parent::__construct();
+    }
+
+    //-------------------------------------------------------------
+    //  The normal pages
+    //-------------------------------------------------------------
+
+    function index() 
+    {
+        $this->data['pagebody'] = 'justone'; // this is the view we want shown
+        $record = $this->quotes->last(); //this will grab the last quote 
+        $this->data = array_merge($this->data, $record);
+        $this->render();
+    }
+}
+/* End of file Last.php */
+/* Location: application/controllers/last/Last.php */
